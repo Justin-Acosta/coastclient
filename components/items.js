e306979ac8a149a53f1d9b25ce1f610108e07dc2
Justin-Acosta <jacostaprof@gmail.com>
2024-08-17 12:39:01 -0500

    deployment prep

--- components/items.js
+++ components/items.js
@@ -5,6 +5,8 @@ import { PlayerInventory } from './player-inventory.js'
 
 export const Items = () => {
 
+    const API_URL = "https://coastapi-3odjm.ondigitalocean.app"
+
     const { token, showPlayerInventory, setShowPlayerInventory, showTackleBox, setShowTackleBox, currentBait } = useAppContext()
 
     const [isExpanded, setIsExpanded] = useState(false);
@@ -44,13 +46,13 @@ export const Items = () => {
                     <div className={`${styles.itemBox} ${styles.tackleBox}`} onClick={toggleShowTackleBox}>
                         <div
                             className={styles.image}
-                            style={{ backgroundImage: `url('http://localhost:8000/media/box/tackle-box.jpg')` }}
+                            style={{ backgroundImage: `url('${API_URL}/media/box/tackle-box.jpg')` }}
                         ></div>
                     </div>
                     <div className={`${styles.itemBox} ${styles.tackleBox}`} onClick={toggleShowPlayerInventory}>
                         <div
                             className={styles.image}
-                            style={{ backgroundImage: `url('http://localhost:8000/media/box/player-inventory.jpg')` }}
+                            style={{ backgroundImage: `url('${API_URL}/media/box/player-inventory.jpg')` }}
                         ></div>
                     </div>
                 </div>
